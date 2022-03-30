6937a53208a5bb9d4a510726a68311a5d94c2150
shaikhsirin <shaikhsirin8080example.com>
2022-03-30 09:30:26 +0530

    Added:- Given Only one card Randmoly

--- src/DistributeCards.java
+++ src/DistributeCards.java
@@ -2,6 +2,7 @@ import java.util.Random;
 public class DistributeCards {
 
     int[][] cards = new int[4][13]; //array of cards
+    private int playerNumber;
 
     //distribute cards to one player
     void DistributeCards(int playerNumber) {
@@ -9,7 +10,18 @@ public class DistributeCards {
             giveCard(playerNumber);
         }
     }
+      //give only one card Randomly
+        void giveCard(int playerNumber) {
 
-    private void giveCard(int playerNumber) {
+            Random random = new Random();
+
+            int suit = random.nextInt(4);
+            int rank = random.nextInt(13);
+
+            if (cards[suit][rank] != 0) {
+                giveCard(playerNumber);
+            } else {
+                cards[suit][rank] = playerNumber;
+            }
+        }
     }
-}
