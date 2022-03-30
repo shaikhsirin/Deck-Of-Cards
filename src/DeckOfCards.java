96383b0c15af5fe1b5ba28c98e5fbdc259080e24
shaikhsirin <shaikhsirin8080example.com>
2022-03-30 09:10:38 +0530

    Added:- Allots the Card for Each Player

--- src/DeckOfCards.java
+++ src/DeckOfCards.java
@@ -1,5 +1,11 @@
 public class DeckOfCards {
     public static void main(String[] args) {
         DistributeCards distributeCards = new DistributeCards(); //class object
+
+        // Cards for each player
+        for (int playerNumber = 1; playerNumber <= 4; playerNumber++) {
+            distributeCards.DistributeCards(playerNumber);
+            System.out.println("Player " + playerNumber + "'s cards:");
+        }
     }
 }
--- src/DistributeCards.java
+++ src/DistributeCards.java
@@ -1,5 +1,15 @@
 import java.util.Random;
 public class DistributeCards {
 
-    int [][] cards = new int[4][13]; //array of cards
+    int[][] cards = new int[4][13]; //array of cards
+
+    //distribute cards to one player
+    void DistributeCards(int playerNumber) {
+        for (int i = 0; i < 9; i++) {
+            giveCard(playerNumber);
+        }
+    }
+
+    private void giveCard(int playerNumber) {
+    }
 }
